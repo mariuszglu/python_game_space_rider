1c65dbd454d8ee56991e0a18b4be90a771b85ef8
Mariusz <pinkiweb88@gmail.com>
2024-02-29 21:20:19 +0100

    create main function and testing run

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pygame
 
 
-WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode(WIDTH, HEIGHT)
+WIDTH, HEIGHT = 1000, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Rider")
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()

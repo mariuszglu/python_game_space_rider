69b8f44dbeee3d03d6ed24a99b33f011b377963b
Mariusz <pinkiweb88@gmail.com>
2024-02-29 21:42:11 +0100

    loading picture on fullcreen

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ WIDTH, HEIGHT = 1000, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Rider")
 
+# BG = pygame.image.load("bg.png")
+#when image is smaller than scrern you can usig transform, scale
+BG = pygame.transform.scale(pygame.image.load("bg.png"),(WIDTH, HEIGHT))
+
+def draw():
+#blit spacial method when you want to put it on the screen
+    WIN.blit(BG,(0,0))
+#update usign every time when you want put changes on the screen
+    pygame.display.update()
+
 def main():
     run = True
     while run:
@@ -12,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw()
 
     pygame.quit()
 

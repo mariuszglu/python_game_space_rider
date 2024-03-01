b90b8f4b42897b59409efc22ef0599aa5e6003aa
Mariusz <pinkiweb88@gmail.com>
2024-03-01 23:16:04 +0100

    moving a character, kreate player (red rectangle)

--- main.py
+++ main.py
@@ -9,20 +9,30 @@ pygame.display.set_caption("Space Rider")
 #when image is smaller than scrern you can usig transform, scale
 BG = pygame.transform.scale(pygame.image.load("bg.png"),(WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+def draw(player):
 #blit spacial method when you want to put it on the screen
     WIN.blit(BG,(0,0))
+#drawing red rectangle (player) on WIN window
+    pygame.draw.rect(WIN,"red",player)
 #update usign every time when you want put changes on the screen
     pygame.display.update()
 
+
+
 def main():
     run = True
+
+# moving a charakter
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw()
+        draw(player)
 
     pygame.quit()
 
